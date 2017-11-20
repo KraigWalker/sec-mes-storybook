35adebdc1f0588174e40b00ec991d96a3cf57bae
Trupti Barole <Trupti@Truptis-MacBook-Pro.local>
2017-11-20 18:12:30 +0000

    form elements added with more scss changes

--- src/js/components/LandingPage.js
+++ src/js/components/LandingPage.js
@@ -4,6 +4,9 @@ import { Link } from 'react-router-dom';
 import { getSecureMessages } from '../actions/AppActions';
 import { connect } from 'react-redux';
 
+//for Textarea input
+import autosize from 'autosize';
+
 /**
  * @class Landing Page 
  * Landing Page of the application
@@ -13,9 +16,20 @@ class LandingPage extends React.PureComponent {
     componentWillMount() {
         !this.props.messages.fetched && this.props.dispatch(getSecureMessages());
     }
+    onChange() {
+        autosize(document.getElementById('ta1'));
+    }
     render() {
         return(
             <div>
+                <div className="c-field">
+                    <label className="c-field__label c-field__label--block" htmlFor="ta1">
+                        Input Textarea
+                    </label>
+                    <div className="c-field__controls">
+                        <textarea className="c-field__input c-field__input--txtarea" name="ta1" id="ta1" rows="10" cols="20" onChange={this.onChange}></textarea>
+                    </div>
+                </div>
                 <ul>
                     <li><Link to = {{ pathname : '/securemessages:inbox', state: this.props.messages }}>Show my messages</Link></li>
                 </ul>
