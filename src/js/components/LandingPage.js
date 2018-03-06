a5d681ad20266826c17af204cb592732251a3863
Akash Srivastava <akash_srivastava2@syntelinc.com>
2018-03-06 17:29:31 +0000

    Back Button added

--- src/js/components/LandingPage.js
+++ src/js/components/LandingPage.js
@@ -4,7 +4,7 @@ import { getActiveTab } from '../actions/AppActions';
 import { connect } from 'react-redux';
 import SecureMessageTabs from './SecureMessageTabs';
 import StepHeader from './common/StepHeader';
-import {SecureMessageBL} from '../bl/SecureMessageBL'
+import { SecureMessageBL } from '../bl/SecureMessageBL'
 import GetIcon from './common/GetIcon';
 import ErrorPage from './common/ErrorPage';
 
@@ -17,29 +17,34 @@ class LandingPage extends React.PureComponent {
     linkClick = (activeTab) => {
         this.props.dispatch(getActiveTab(activeTab));
     }
-    mapMessages(messages){
+    mapMessages(messages) {
         return SecureMessageBL(messages);
     }
     checkError() {
-        if(this.props.messages.error && this.props.messages.fetched) {
+        if (this.props.messages.error && this.props.messages.fetched) {
             this.props.history.push("/errormessage");
-         } else {
-             return (<div>
-                 <div className="row">
+        } else {
+            return (<div>
+                <div className="row">
                     <div className="col-md1-18">
-                        <StepHeader showheaderCrumbs={false} headerTitle={this.props.content.landingPageTitle} headerSubtext={this.props.content.landingPageMessage}/>
+                        <StepHeader showheaderCrumbs={false} headerTitle={this.props.content.landingPageTitle} headerSubtext={this.props.content.landingPageMessage} />
                     </div>
-                </div> <Link className="c-btn c-btn--default u-margin-bottom-c new-message-btn" to = {{ pathname : `${window.baseURl}/newsecuremessage` }}>
-             <GetIcon id="icon-pencil" width="16px" height="16px" />New secure message
+                </div> <Link className="c-btn c-btn--default u-margin-bottom-c new-message-btn" to={{ pathname: `${window.baseURl}/newsecuremessage` }}>
+                    <GetIcon id="icon-pencil" width="16px" height="16px" />New secure message
          </Link>
-         <SecureMessageTabs location={this.props.location} onClick={this.linkClick}  messages={this.mapMessages(this.props.messages)} activeTab={this.props.activeTab}/>
-         </div>)
-         }
-     }
+                <SecureMessageTabs location={this.props.location} onClick={this.linkClick} messages={this.mapMessages(this.props.messages)} activeTab={this.props.activeTab} />
+            </div>)
+        }
+    }
     render() {
-        return(
+        return (
             <div className="container">
-              {this.checkError()}
+                <div className="row">
+                    <div className="col-md1-18">
+                        <StepHeader showheaderCrumbs={true} onClick={() => { }} headerCrumbsMessage="Back" headerTitle="New message" headerCrumbsPath={{ pathname: `${window.baseURl}/securemessage` }} />
+                    </div>
+                </div>
+                {this.checkError()}
             </div>
         );
     }
@@ -49,7 +54,7 @@ class LandingPage extends React.PureComponent {
  * @param {object} state. State of the application
  */
 
-const mapState = (state) => { 
+const mapState = (state) => {
     return {
         messages: state.messages,
         accounts: state.accounts,
