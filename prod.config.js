9a57bb8aaba22b7b47c8199e6727182483c101c8
Binod Singh <binod.singh@cybg.com>
2018-03-09 12:57:35 +0000

    test for images

--- prod.config.js
+++ prod.config.js
@@ -3,15 +3,15 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const path = require('path');
 
 const brand = process.env.brand;
-const brandEntry = ["babel-polyfill","./src/scss/main.scss"];
-const noBrandEntry = ["babel-polyfill","./src/js/client.js"];
+const brandEntry = ["babel-polyfill", "./src/scss/main.scss"];
+const noBrandEntry = ["babel-polyfill", "./src/js/client.js"];
 
 console.log(brand);
 
 module.exports = {
-    entry: brand?brandEntry:noBrandEntry,
+    entry: brand ? brandEntry : noBrandEntry,
     output: {
-        path:__dirname+ '/src/compiled/',
+        path: __dirname + '/src/compiled/',
         filename: "[name].bundle.js",
         publicPath: '/compiled/'
     },
@@ -19,50 +19,50 @@ module.exports = {
         rules: [
             {
                 test: /\.jsx?$/,
-                exclude:/node_modules/,
+                exclude: /node_modules/,
                 loader: 'babel-loader',
                 query: {
-                presets: ['react', 'es2015', 'stage-0'],
-                plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
+                    presets: ['react', 'es2015', 'stage-0'],
+                    plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
                 }
             },
             {
-            test: /\.scss$/,
-            exclude: /node_modules/,
-            loader: ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                use: [
-                    {
-                        loader: 'css-loader',
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            data: "$brand: "+brand+";$env: prod;"
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                loader: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        {
+                            loader: 'css-loader',
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                data: "$brand: " + brand + ";$env: prod;"
+                            }
                         }
-                    }
-                ]
-            }),
+                    ]
+                }),
             },
             {
-            test: /\.json$/,
-            loader: 'json-loader'
+                test: /\.json$/,
+                loader: 'json-loader'
             },
             {
                 test: /\.(otf|ttf|eot|svg)$/,
-                exclude: ['/node_modules/','src/images/'],
+                exclude: ['/node_modules/', 'src/images/'],
                 include: resolve(__dirname, "src/fonts/"),
                 loader: 'url-loader?limit=1024&name=fonts/[name].[ext]'
             },
             {
                 test: /\.(jpg|jpeg|gif|png|svg)$/,
-                exclude: /node_modules/,
+                exclude: ['/node_modules/', 'src/fonts/'],
                 include: resolve(__dirname, "src/images/"),
-                loader:'url-loader?limit=1024&name=/images/[name].[ext]'
+                loader: 'url-loader?limit=1024&name=/images/[name].[ext]'
             }
         ]
     },
     plugins: [
-        new ExtractTextPlugin(brand+'.[name].css', { allChunks: true })
+        new ExtractTextPlugin(brand + '.[name].css', { allChunks: true })
     ],
 };
