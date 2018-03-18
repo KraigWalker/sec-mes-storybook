f9a96745df87c8083a9d3a8ff27f7c61aed215a7
Akash Srivastava <akash_srivastava2@syntelinc.com>
2018-03-18 22:51:15 +0000

    Base url changes

--- src/js/api/ApiUtils.js
+++ src/js/api/ApiUtils.js
@@ -1,57 +1,83 @@
-import axios from 'axios';
+import axios from "axios";
 //import { getClientContext, getClientContextCB } from './ApiHeaders';
-import token from '../token.js';
-const apiParams  = {
-    "accessToken": token.accessToken(),
-    "clientContext": token.getClientContext(),
-    "bankId" :  token.getBankId()
-  };
+import token from "../token.js";
+import config from "../config";
+const apiParams = {
+  accessToken: token.accessToken(),
+  clientContext: token.getClientContext(),
+  bankId: token.getBankId()
+};
 
 const requestHeaders = {
-    'x-bpi-client-context': JSON.stringify(apiParams.clientContext),
-    'x-bpi-version': '1.2.0',
-    'Authorization': apiParams.accessToken,
-    'Content-Type': 'application/json',
+  "x-bpi-client-context": JSON.stringify(apiParams.clientContext),
+  "x-bpi-version": "1.2.0",
+  Authorization: apiParams.accessToken,
+  "Content-Type": "application/json"
 };
 
 const accountRequestHeader = {
-    'x-bpi-client-context': JSON.stringify(apiParams.clientContext),
-    'x-bpi-version': '0.8.0',
-    'Authorization': apiParams.accessToken,
-    'Content-Type': 'application/json',
-}
-
+  "x-bpi-client-context": JSON.stringify(apiParams.clientContext),
+  "x-bpi-version": "0.8.0",
+  Authorization: apiParams.accessToken,
+  "Content-Type": "application/json"
+};
 
 class ApiUtils {
-    static makeRequest(apiData, onSuccess, onFail) {
-        apiData.url = (apiData.url).replace('{bank_id}', apiParams.bankId);
-        switch (apiData.method) {
-            case 'GET':
-                if (apiData.url !== `https://my-dev.cybservices.co.uk/bpiInt3/banks/${apiParams.bankId}/accounts/default`) {
-                    return axios.get(apiData.url, { headers: requestHeaders })
-                        .then(response => { onSuccess(response.data); })
-                        .catch(error => { onFail(error); });
-                }
-                else return axios.get(apiData.url, { headers: accountRequestHeader })
-                    .then(response => { onSuccess(response.data); })
-                    .catch(error => { onFail(error); });
+  static makeRequest(apiData, onSuccess, onFail) {
+    apiData.url = apiData.url.replace("{bank_id}", apiParams.bankId);
+    switch (apiData.method) {
+      case "GET":
+        if (
+          apiData.url !==
+          `${config.apiBaseUrl}/banks/${apiParams.bankId}/accounts/default`
+        ) {
+          return axios
+            .get(apiData.url, { headers: requestHeaders })
+            .then(response => {
+              onSuccess(response.data);
+            })
+            .catch(error => {
+              onFail(error);
+            });
+        } else
+          return axios
+            .get(apiData.url, { headers: accountRequestHeader })
+            .then(response => {
+              onSuccess(response.data);
+            })
+            .catch(error => {
+              onFail(error);
+            });
 
-            case 'POST':
-                return axios.post(apiData.url, apiData.requestData, { headers: requestHeaders })
-                    .then(response => {
-                        onSuccess(response);
-                    })
-                    .catch(error => { onFail(error); });
-            case 'PUT':
-                return axios.put(apiData.url, apiData.requestData, { headers: requestHeaders })
-                    .then(response => { onSuccess(response); })
-                    .catch(error => { onFail(error); });
-            case 'DELETE':
-                return axios.delete(apiData.url, apiData.requestData, { headers: requestHeaders })
-                    .then(response => { onSuccess(response.data); })
-                    .catch(error => { onFail(error); });
-        }
+      case "POST":
+        return axios
+          .post(apiData.url, apiData.requestData, { headers: requestHeaders })
+          .then(response => {
+            onSuccess(response);
+          })
+          .catch(error => {
+            onFail(error);
+          });
+      case "PUT":
+        return axios
+          .put(apiData.url, apiData.requestData, { headers: requestHeaders })
+          .then(response => {
+            onSuccess(response);
+          })
+          .catch(error => {
+            onFail(error);
+          });
+      case "DELETE":
+        return axios
+          .delete(apiData.url, apiData.requestData, { headers: requestHeaders })
+          .then(response => {
+            onSuccess(response.data);
+          })
+          .catch(error => {
+            onFail(error);
+          });
     }
+  }
 }
 
 export default ApiUtils;
