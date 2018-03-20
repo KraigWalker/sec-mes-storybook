36cc242713459e89f84073ca56ae5681a267cb05
Akash Srivastava <akash_srivastava2@syntelinc.com>
2018-03-20 01:06:02 +0000

    BOW-4787

--- src/js/entities/MessageAccountEntity.js
+++ src/js/entities/MessageAccountEntity.js
@@ -4,10 +4,10 @@ class MessageAccountEntity {
             this.accounts = [];
     }
     setAccounts(account) {
-        this.accounts.push({"name":account.product.name,"number":account.number, "accountId": account.id});
+        this.accounts.push({"name":account.product.name,"number":account.number, "accountId": account.id, "display_order": account.metadata.display_order});
     }
     getAccounts() {
-        return this.accounts;
+        return _.sortBy(this.accounts, ['display_order']);
     }
 }
 export default MessageAccountEntity;
