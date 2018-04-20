d3bf08b934a29f7734caf911fc0ebf47241266ab
Akash Srivastava <Akash_Srivastava2@syntelinc.com>
2018-04-20 09:43:30 +0100

    Merging Test Case branch

--- src/js/components/tests/CalloutComponent.spec.js
+++ src/js/components/tests/CalloutComponent.spec.js
@@ -3,9 +3,9 @@ import content from '../../content';
 import { shallow, mount } from 'enzyme';
 import CalloutComponent from '../common/CalloutComponent'
 import Enzyme from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+// import Adapter from "enzyme-adapter-react-16";
 
-Enzyme.configure({ adapter: new Adapter() });
+// Enzyme.configure({ adapter: new Adapter() });
 
 describe('CalloutComponent Component Check', () => {
 
