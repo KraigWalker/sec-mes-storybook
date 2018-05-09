5659cbf3ded39e0d7dd5315dc536767353029590
Akash_Srivastava2@syntelinc.com <Akash_Srivastava2@syntelinc.com>
2018-05-09 10:36:52 +0100

    Commented fingerprints

--- src/js/components/LandingPage.js
+++ src/js/components/LandingPage.js
@@ -21,7 +21,7 @@ export class LandingPage extends React.PureComponent {
         const FPOptions = { excludePixelRatio: true, excludeScreenResolution: true };
         new Fingerprint(FPOptions).get(result => {
             if (this.props.fingerprintID !== result) {
-                 this.props.history.push('/errormessage');
+             //    this.props.history.push('/errormessage'); TODOxCYBG
             }
         });
         window.top.postMessage('clearNewMessagePage', '*');
