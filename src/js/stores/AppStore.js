ebedff2d01f3744b321f11d35a079efd90e85df8
R908065 <Ryan.Vincent@Screenmedia.co.uk>
2019-02-07 15:48:06 +0000

    fix: base url and bump doc version

--- src/js/stores/AppStore.js
+++ src/js/stores/AppStore.js
@@ -5,7 +5,7 @@ import { buildMiddleware } from "document-management-web-ui";
 import reducer from "../reducers";
 
 const middleware = (session, clientContext, config) => applyMiddleware(
-    buildMiddleware(clientContext, session, { apiBaseUrl: config.apiBaseUrl3 })
+    buildMiddleware(clientContext, session, { apiBaseUrl: config.apiBaseUrl2 })
 )
 
 export default (session, clientContext, config) => createStore(reducer, middleware(session, clientContext, config))
