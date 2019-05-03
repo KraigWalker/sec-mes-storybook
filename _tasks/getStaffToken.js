a2610d83684578dc5e38de40d572fa06b3fe51ee
Joe Morisson <joe.morrison@screenmedia.co.uk>
2019-05-03 14:58:54 +0100

    fixed based on feedback, removed node-sass

--- _tasks/getStaffToken.js
+++ _tasks/getStaffToken.js
@@ -1,37 +1,43 @@
-const fs = require("fs");
-const inquirer = require("inquirer");
-const {staffToken} = require("./loginToMEO");
-const advisorHtmlPath = `${process.cwd()}/dev/advisor/`;
-const questions = [{
-    type: "input",
-    name: "username",
-    message: "what is your ALP username",
-    default: false,
-}, {
-    type: "password",
-    name: "password",
-    message: "what is your ALP password",
-    mask: true,
-    default: false,
-}];
+try {
+    const fs = require("fs");
+    const inquirer = require("inquirer");
+    const {staffToken} = require("./loginToMEO");
+    const advisorHtmlPath = `${process.cwd()}/dev/advisor/`;
+    const questions = [{
+        type: "input",
+        name: "username",
+        message: "what is your ALP username",
+        default: false,
+    }, {
+        type: "password",
+        name: "password",
+        message: "what is your ALP password",
+        mask: true,
+        default: false,
+    }];
 // Run developer login enquiries
-inquirer
-    .prompt(questions)
-    .then(({username, password}) => staffToken(username, password)
-        .then(token => {
-            if (username && password) {
-                return fs.readFile(`${advisorHtmlPath}/index_tmp.html`, 'utf-8', (err, contents) => {
-                    if (err) throw err;
-                    const htmlWithToken = contents.toString()
-                        .replace("{{staffToken}}", token);
-                    fs.writeFile(`${advisorHtmlPath}/index.html`, htmlWithToken, (err) => {
-                        if (err) {
-                            throw console.log(err);
-                        }
-                        console.log("file saved!");
-                    });
+    inquirer
+        .prompt(questions)
+        .then(({username, password}) => staffToken(username, password)
+            .then(token => {
+                if (username && password) {
+                    return fs.readFile(`${advisorHtmlPath}/index_tmp.html`, 'utf-8', (err, contents) => {
+                        if (err) throw err;
+                        const htmlWithToken = contents.toString()
+                            .replace("{{staffToken}}", token);
+                        fs.writeFile(`${advisorHtmlPath}/index.html`, htmlWithToken, (err) => {
+                            if (err) {
+                                throw console.log(err);
+                            }
+                            console.log("file saved!");
+                        });
 
-                });
-            }
-        }).catch(e => console.log(e))
-        .catch(err => console.log(err)));
+                    });
+                }
+            }).catch(e => console.log(e))
+            .catch(err => console.log(err)));
+} catch (err) {
+    // ASYNC/AWAIT won't work on early node versions
+    console.warn("Check that your node version is >= 10. re-run npm install.");
+    throw err;
+}
--- _tasks/openMEOBrowser.js
+++ _tasks/openMEOBrowser.js
@@ -2,21 +2,15 @@ const shell = require("shelljs");
 const macChromePath = "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome";
 const instructionPath = "/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome --ignore-certificate-errors";
 
-try {
-    const hasCurlRequest = require("curl-request");
-    if (hasCurlRequest) {
-        // TODO: find windows paths
-        if (shell.which(macChromePath)) {
-            shell.exec("echo Please close all active Google Chrome instances.");
-            shell.exec("echo Will wait 5 seconds...");
-            shell.exec("sleep 5");
-            shell.exec(`${instructionPath} > /dev/null $`, {async: true});
-            return shell.exit(0);
-        }
-        return shell.exec("echo Error Google Chrome application path not detected on your machine. This must be started with ignore certicate errors.").stdout && shell.exit(1);
-    }
-} catch (err) {
-    console.error("ERROR Please use node >= 10 to run MEO advisor dependencies. Re-run npm install once on node >= 10");
-    throw err;
+// TODO: find windows paths
+if (shell.which(macChromePath)) {
+    shell.exec("echo Please close all active Google Chrome instances.");
+    shell.exec("echo Will wait 5 seconds...");
+    shell.exec("sleep 5");
+    shell.exec(`${instructionPath} > /dev/null $`, {async: true});
+    return shell.exit(0);
 }
+return shell.exec("echo Error Google Chrome application path not detected on your machine. This must be started with ignore certicate errors.").stdout && shell.exit(1);
+
+
 
