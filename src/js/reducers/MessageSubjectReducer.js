211aa3927c981f2ac0f48cc7437d6c76a9958377
Joe Morisson <joe.morrison@screenmedia.co.uk>
2019-05-21 11:01:12 +0100

    temp fix until back end work done to strip out New Document

--- src/js/reducers/MessageSubjectReducer.js
+++ src/js/reducers/MessageSubjectReducer.js
@@ -1,40 +1,46 @@
 import AppConstants from '../constants/AppConstants';
+import {EXCLUDED_SUBJECTS} from "../constants/StringsConstants";
+
 /**
- * 
- * @param {*} state 
- * @param {*} action 
+ *
+ * @param {*} state
+ * @param {*} action
  */
 
-function reducer(state={
+function reducer(state = {
     subjects: [],
     fetching: false,
     fetched: false,
     error: false,
 }, action) {
 
-  switch (action.type) {
-    case AppConstants.REQUEST_SECURE_MESSAGES: {
-      return {...state, fetching: true }
+    switch (action.type) {
+        case AppConstants.REQUEST_SECURE_MESSAGES: {
+            return {...state, fetching: true}
+        }
+        case AppConstants.REQUEST_SUBJECTS_SUCCESS: {
+            return {
+                ...state, fetching: false, fetched: true, subjects: action.payload
+                    .filter(({key}) => EXCLUDED_SUBJECTS.indexOf(key) < 0)
+            }
+        }
+        case AppConstants.REQUEST_SUBJECTS_FAILURE: {
+            return {...state, error: true, fetched: true}
+        }
+        case AppConstants.SET_POPUP_STATE: {
+            return {...state, error: false}
+        }
+        default:
+            return state;
     }
-    case AppConstants.REQUEST_SUBJECTS_SUCCESS: {
-      return {...state, fetching : false, fetched: true, subjects: action.payload}
-    }
-    case AppConstants.REQUEST_SUBJECTS_FAILURE: {
-      return {...state, error : true, fetched: true}
-    }
-    case AppConstants.SET_POPUP_STATE: {
-      return {...state,error: false}
-    }
-   default:
-    return state;
-  }
 }
+
 export default reducer;
 
 const getSubjects = (state) => state.subjects;
 const getSubjectErrors = (state) => state.error;
 
 export const selectors = {
-  getSubjects,
-  getSubjectErrors
+    getSubjects,
+    getSubjectErrors
 }
