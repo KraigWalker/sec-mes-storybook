4b64aec31bfc2bb1fba988bc89283b4fef78df47
Joe Morisson <joe.morrison@screenmedia.co.uk>
2019-05-24 11:42:12 +0100

    added unit test covering cases of computed properties of the tab groups in the mail component

--- src/js/components/__tests__/SecureMessageTabs.spec.js
+++ src/js/components/__tests__/SecureMessageTabs.spec.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { SecureMessageTabs } from '../SecureMessageTabs';
-import { shallow } from 'enzyme';
+import {SecureMessageTabs} from '../SecureMessageTabs';
+import {shallow} from 'enzyme';
+import {TabButton} from ""
 
 describe("New Secure message snapshot", () => {
     const dispatch = jest.fn();
@@ -18,12 +19,22 @@ describe("New Secure message snapshot", () => {
         dispatch: dispatch
     };
     let component = shallow(<SecureMessageTabs {...props} />);
+    const tabs = [{id: "INBOX", title: "Inbox (1)"}, {id: "DRAFT", title: "Drafts"}, {
+        id: "SENT",
+        title: "Sent"
+    }, {id: "ARCHIVED", title: "Archive"}];
     it('should match to snapshot', () => {
         expect(component).toMatchSnapshot();
+
+    });
+    it("renders the tab groups", () => {
+        const {tabButtons} = component.find("TabGroup").props();
+        expect(Array.isArray(tabButtons)).toBe(true);
+        expect(tabButtons).toEqual(tabs);
     });
     it('onClick method test', () => {
         component.instance().onclick();
-        component.setState({ tab: 'inbox' });
+        component.setState({tab: 'inbox'});
         expect(component.instance().state.tab).toBe('inbox');
     });
 });
