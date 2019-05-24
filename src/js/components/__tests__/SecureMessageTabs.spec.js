ef1885741dcf14c411867c238ce3992705460e4c
Joe Morisson <joe.morrison@screenmedia.co.uk>
2019-05-24 11:49:17 +0100

    added unit test covering cases of computed properties of the tab groups in the mail component

--- src/js/components/__tests__/SecureMessageTabs.spec.js
+++ src/js/components/__tests__/SecureMessageTabs.spec.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {SecureMessageTabs} from '../SecureMessageTabs';
 import {shallow} from 'enzyme';
-import {TabButton} from ""
 
 describe("New Secure message snapshot", () => {
     const dispatch = jest.fn();
@@ -31,7 +30,8 @@ describe("New Secure message snapshot", () => {
         const {tabButtons} = component.find("TabGroup").props();
         expect(Array.isArray(tabButtons)).toBe(true);
         expect(tabButtons).toEqual(tabs);
-    });
+    });.
+
     it('onClick method test', () => {
         component.instance().onclick();
         component.setState({tab: 'inbox'});
