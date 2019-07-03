a8fadaa721a292177a06a12e8266407f1ecd7bf8
Joe Morisson <joe.morrison@screenmedia.co.uk>
2019-07-03 14:51:51 +0100

    Add a singleton instance of with messaging to make sure only one modal appears in multiple HOCs

--- src/js/components/common/WithMessaging.js
+++ src/js/components/common/WithMessaging.js
@@ -1,259 +1,278 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { compose } from "redux";
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import {compose} from "redux";
 import {
-  updateMessageData,
-  delMessageData,
-  closeDelModal,
-  archiveMessageData,
-  unarchiveMessageData,
-  popupState
+    updateMessageData,
+    delMessageData,
+    closeDelModal,
+    archiveMessageData,
+    unarchiveMessageData,
+    popupState
 } from "../../actions/AppActions";
 import {
-  NEW,
-  READ,
-  DELETED,
-  READ_ONLY,
-  ARCHIVED
+    NEW,
+    READ,
+    DELETED,
+    READ_ONLY,
+    ARCHIVED
 } from "../../constants/StringsConstants";
 import {
-  DELETE_MODAL,
-  ARCHIVE_MODAL,
-  UNARCHIVE_MODAL
+    DELETE_MODAL,
+    ARCHIVE_MODAL,
+    UNARCHIVE_MODAL
 } from "../../constants/ModalConstants";
-import { withRouter } from "react-router-dom";
+import {withRouter} from "react-router-dom";
 import PropTypes from "prop-types";
-import { ConfirmationModal } from "web-ui-components/lib/organisms/modals";
+import {ConfirmationModal} from "web-ui-components/lib/organisms/modals";
 import getOptionDisplayFunctions from "./MessageOptions";
-import { TextBody } from "web-ui-components/lib/atoms/text";
+import {TextBody} from "web-ui-components/lib/atoms/text";
 import SuccessModal from "./SuccessModal";
 import ErrorModal from "./ErrorModal";
 
 const getSuccessModalMessage = (modalType, content) => {
-  switch (modalType) {
-    case ARCHIVE_MODAL: {
-      return content.messageArchived;
+    switch (modalType) {
+        case ARCHIVE_MODAL: {
+            return content.messageArchived;
+        }
+        case UNARCHIVE_MODAL: {
+            return content.messageUnarchived;
+        }
+        case DELETE_MODAL:
+        default: {
+            return content.messageDeleted;
+        }
     }
-    case UNARCHIVE_MODAL: {
-      return content.messageUnarchived;
-    }
-    case DELETE_MODAL:
-    default: {
-      return content.messageDeleted;
-    }
-  }
 };
 
 const WithMessaging = WrappedComponent =>
-  class withMessaging extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        showDeleteConfirmModal: false,
-        showSendServiceErrorModal: false,
-        messageToDelete: {}
-      };
+    class withMessaging extends Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                showDeleteConfirmModal: false,
+                showSendServiceErrorModal: false,
+                modalType: 0,
+                showSuccessModal: false,
+                messageToDelete: {}
+            };
+            // singleton instance
+            withMessaging.__singletonRef = this;
+            // end of singleton
+            this.showDeleteConfirm = this.showDeleteConfirm.bind(this);
+            this.closeConfirmModal = this.closeConfirmModal.bind(this);
+            this.closeSuccessModal = this.closeSuccessModal.bind(this);
+            this.closeErrorClicked = this.closeErrorClicked.bind(this);
+            this.deleteClick = this.deleteClick.bind(this);
+            this.archiveClick = this.archiveClick.bind(this);
+            this.unarchiveClick = this.unarchiveClick.bind(this);
+            this.replyClick = this.replyClick.bind(this);
+            this.returnErrorModal = this.returnErrorModal.bind(this);
+            this.getDeleteConfirmModal = this.getDeleteConfirmModal.bind(this);
+            this.closeAndReturn = this.closeAndReturn.bind(this);
+            this.__show = this.__show.bind(this);
+            this.__hide = this.__show.bind(this);
+        }
 
-      this.showDeleteConfirm = this.showDeleteConfirm.bind(this);
-      this.closeConfirmModal = this.closeConfirmModal.bind(this);
-      this.closeSuccessModal = this.closeSuccessModal.bind(this);
-      this.closeErrorClicked = this.closeErrorClicked.bind(this);
-      this.deleteClick = this.deleteClick.bind(this);
-      this.archiveClick = this.archiveClick.bind(this);
-      this.unarchiveClick = this.unarchiveClick.bind(this);
-      this.replyClick = this.replyClick.bind(this);
-      this.returnSuccessModalComponent = this.returnSuccessModalComponent.bind(
-        this
-      );
-      this.returnErrorModal = this.returnErrorModal.bind(this);
-      this.getDeleteConfirmModal = this.getDeleteConfirmModal.bind(this);
-      this.closeAndReturn = this.closeAndReturn.bind(this);
-    }
+        getOptionFunctions() {
+            return getOptionDisplayFunctions(
+                this.props.readOnly,
+                this.props.message.noReply
+            );
+        }
 
-    getOptionFunctions() {
-      return getOptionDisplayFunctions(
-        this.props.readOnly,
-        this.props.message.noReply
-      );
-    }
+        componentDidMount() {
+            this.props.popupState();
+        }
 
-    componentDidMount() {
-      this.props.popupState();
-    }
+        closeAndReturn() {
+            this.closeSuccessModal();
+            const {pathName} = this.props.location;
+            if (pathName !== "/securemessages") {
+                this.props.history.push("/securemessages");
+            }
+        }
 
-    closeAndReturn() {
-      this.closeSuccessModal();
-      const { pathName } = this.props.location;
-      if (pathName !== "/securemessages") {
-        this.props.history.push("/securemessages");
-      }
-    }
+        returnErrorModal() {
+            const {content} = this.props;
 
-    returnSuccessModalComponent(modalType, content) {
-      return (
-        <SuccessModal
-          onClick={this.closeAndReturn}
-          bodyText={getSuccessModalMessage(modalType, content)}
-          okText={content.ok}
-        />
-      );
-    }
+            return (
+                <ErrorModal
+                    content={content}
+                    onClose={this.errorCloseClicked}
+                    onConfirm={this.retryServiceCall}
+                />
+            );
+        }
 
-    returnErrorModal() {
-      const { content } = this.props;
+        archiveClick(message) {
+            this.props.archiveMessageData(message, message.id, ARCHIVED);
+        }
 
-      return (
-        <ErrorModal
-          content={content}
-          onClose={this.errorCloseClicked}
-          onConfirm={this.retryServiceCall}
-        />
-      );
-    }
+        unarchiveClick(message) {
+            this.props.unarchiveMessageData(message, message.id, READ);
+        }
 
-    archiveClick(message) {
-      this.props.archiveMessageData(message, message.id, ARCHIVED);
-    }
+        replyClick(message) {
+            this.props.history.push({
+                pathname: "/securemessages/reply",
+                messageDetail: message,
+                backpath: "/securemessages/"
+            });
+        }
 
-    unarchiveClick(message) {
-      this.props.unarchiveMessageData(message, message.id, READ);
-    }
+        deleteClick() {
+            const message = this.state.messageToDelete;
+            if (message.status === NEW) {
+                this.props.updateMessageData(message, message.id, READ);
+                setTimeout(() => {
+                    this.props.delMessageData(message, message.id, DELETED);
+                }, 500);
+            } else this.props.delMessageData(message, message.id, DELETED);
+            this.setState({
+                showDeleteConfirmModal: false,
+                showSendServiceErrorModal: true
+            });
+        }
 
-    replyClick(message) {
-      this.props.history.push({
-        pathname: "/securemessages/reply",
-        messageDetail: message,
-        backpath: "/securemessages/"
-      });
-    }
+        retryServiceCall() {
+            this.props.popupState();
+            this.deleteClick();
+        }
 
-    deleteClick() {
-      const message = this.state.messageToDelete;
-      if (message.status === NEW) {
-        this.props.updateMessageData(message, message.id, READ);
-        setTimeout(() => {
-          this.props.delMessageData(message, message.id, DELETED);
-        }, 500);
-      } else this.props.delMessageData(message, message.id, DELETED);
-      this.setState({
-        showDeleteConfirmModal: false,
-        showSendServiceErrorModal: true
-      });
-    }
+        closeConfirmModal() {
+            this.setState({showDeleteConfirmModal: false, messageToDelete: null});
+        }
 
-    retryServiceCall() {
-      this.props.popupState();
-      this.deleteClick();
-    }
+        closeSuccessModal() {
+            this.props.closeDelModal();
+        }
 
-    closeConfirmModal() {
-      this.setState({ showDeleteConfirmModal: false, messageToDelete: null });
-    }
+        closeErrorClicked() {
+            this.props.popupState();
+            this.setState({showSendServiceErrorModal: false});
+        }
 
-    closeSuccessModal() {
-      this.props.closeDelModal();
-    }
+        showDeleteConfirm(message) {
+            this.setState({
+                showDeleteConfirmModal: true,
+                messageToDelete: {...message}
+            });
+        }
 
-    closeErrorClicked() {
-      this.props.popupState();
-      this.setState({ showSendServiceErrorModal: false });
-    }
+        getDeleteConfirmModal(message) {
+            const {content} = this.props;
 
-    showDeleteConfirm(message) {
-      this.setState({
-        showDeleteConfirmModal: true,
-        messageToDelete: { ...message }
-      });
-    }
+            return (
+                <ConfirmationModal
+                    title={content.delete}
+                    onConfirm={() => this.deleteClick(message)}
+                    isOpen={true}
+                    onClose={this.closeConfirmModal}
+                    dismissButtonText={content.dontDelButton}
+                    confirmButtonText={content.delButton}
+                >
+                    <TextBody>{content.deleteMessageBody}</TextBody>
+                </ConfirmationModal>
+            );
+        }
 
-    getDeleteConfirmModal(message) {
-      const { content } = this.props;
+        componentWillReceiveProps(nextProps, nextContext) {
+            const {modalType} = this.state;
+            const {messageDetail} = nextProps;
+            if (modalType !== messageDetail.modalType) {
+                return withMessaging.show()
+            }
+        }
+        static show () {
+            withMessaging.__singletonRef.__show();
+        }
+        static hide () {
+            withMessaging.__singletonRef.__hide();
+        }
+        __show() {
+            this.setState({
+                showSuccessModal: true,
+            });
+        }
 
-      return (
-        <ConfirmationModal
-          title={content.delete}
-          onConfirm={() => this.deleteClick(message)}
-          isOpen={true}
-          onClose={this.closeConfirmModal}
-          dismissButtonText={content.dontDelButton}
-          confirmButtonText={content.delButton}
-        >
-          <TextBody>{content.deleteMessageBody}</TextBody>
-        </ConfirmationModal>
-      );
-    }
+        __hide() {
+            this.setState({
+                showSuccessModal: false,
+            });
+        }
 
-    render() {
-      const {
-        messageDetail,
-        messages,
-        content,
-        message,
-        viewMessageFlag
-      } = this.props;
-
-      const optionFunctions = this.getOptionFunctions();
-      const showDelete = optionFunctions.showDeleteButton(message.status);
-      const showReply = optionFunctions.showReplyButton(message.status);
-      const showArchive = optionFunctions.showArchiveButton(message.status);
-      const showUnarchive = optionFunctions.showUnarchiveButton(message.status);
-
-      return (
-        <div className="u-full-width">
-          <WrappedComponent
-            {...this.props}
-            onArchiveClick={showArchive ? this.archiveClick : null}
-            onReplyClick={showReply ? this.replyClick : null}
-            onDeleteClick={showDelete ? this.showDeleteConfirm : null}
-            onUnarchiveClick={showUnarchive ? this.unarchiveClick : null}
-            showArchive={showArchive}
-            showDelete={showDelete}
-            showUnarchive={showUnarchive}
-            showReply={showReply}
-          />
-          {this.state.showDeleteConfirmModal && this.getDeleteConfirmModal()}
-          {messageDetail.modalType > 0 &&
-            this.returnSuccessModalComponent(messageDetail.modalType, content)}
-          {messages.draftError &&
-            this.state.showSendServiceErrorModal &&
-            this.returnErrorModal()}
-        </div>
-      );
-    }
-  };
+        render() {
+            const {
+                messageDetail,
+                messages,
+                content,
+                message,
+            } = this.props;
+            const {showSuccessModal} = this.state;
+            const optionFunctions = this.getOptionFunctions();
+            const showDelete = optionFunctions.showDeleteButton(message.status);
+            const showReply = optionFunctions.showReplyButton(message.status);
+            const showArchive = optionFunctions.showArchiveButton(message.status);
+            const showUnarchive = optionFunctions.showUnarchiveButton(message.status);
+            return (
+                <div className="u-full-width">
+                    <WrappedComponent
+                        {...this.props}
+                        onArchiveClick={showArchive ? this.archiveClick : null}
+                        onReplyClick={showReply ? this.replyClick : null}
+                        onDeleteClick={showDelete ? this.showDeleteConfirm : null}
+                        onUnarchiveClick={showUnarchive ? this.unarchiveClick : null}
+                        showArchive={showArchive}
+                        showDelete={showDelete}
+                        showUnarchive={showUnarchive}
+                        showReply={showReply}
+                    />
+                    {this.state.showDeleteConfirmModal && this.getDeleteConfirmModal()}
+                    {showSuccessModal && <SuccessModal
+                        onClick={this.closeAndReturn}
+                        bodyText={getSuccessModalMessage(messageDetail.modalType, content)}
+                        okText={content.ok}
+                    />}
+                    {messages.draftError &&
+                    this.state.showSendServiceErrorModal &&
+                    this.returnErrorModal()}
+                </div>
+            );
+        }
+    };
 
 WithMessaging.propTypes = {
-  viewMessageFlag: PropTypes.bool,
-  content: PropTypes.object,
-  messages: PropTypes.array,
-  messageDetail: PropTypes.object
+    viewMessageFlag: PropTypes.bool,
+    content: PropTypes.object,
+    messages: PropTypes.array,
+    messageDetail: PropTypes.object
 };
 
 const mapState = (state, props) => ({
-  messages: state.messages,
-  messagesubjects: state.subjects,
-  accounts: state.accounts,
-  messageDetail: state.viewMessage,
-  readOnly: state.messages.mode === READ_ONLY,
-  message: props.message ? props.message : props.location.messageDetail
+    messages: state.messages,
+    messagesubjects: state.subjects,
+    accounts: state.accounts,
+    messageDetail: state.viewMessage,
+    readOnly: state.messages.mode === READ_ONLY,
+    message: props.message ? props.message : props.location.messageDetail
 });
 
 const actionCreators = {
-  delMessageData,
-  updateMessageData,
-  closeDelModal,
-  popupState,
-  unarchiveMessageData,
-  archiveMessageData
+    delMessageData,
+    updateMessageData,
+    closeDelModal,
+    popupState,
+    unarchiveMessageData,
+    archiveMessageData
 };
 
 const composedWithMessaging = compose(
-  connect(
-    mapState,
-    actionCreators
-  ),
-  withRouter,
-  WithMessaging
+    connect(
+        mapState,
+        actionCreators
+    ),
+    withRouter,
+    WithMessaging
 );
 
 export default composedWithMessaging;
