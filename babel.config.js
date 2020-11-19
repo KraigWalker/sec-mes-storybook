3f7bfedab790aaae811f0ac9f772b1b6ad4302a2
Kraig Walker <kraig.walker@cybg.com>
2020-11-19 14:46:02 +0000

    fix jest incompatibilities

---
+++ babel.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  presets: [
+    '@babel/preset-react',
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          browsers: [
+            'IE 11',
+            'last 3 Chrome versions',
+            'last 2 Edge versions',
+            'last 2 Firefox versions',
+            'last 2 Safari versions',
+            'last 2 ChromeAndroid versions',
+            'last 2 iOS versions',
+          ],
+        },
+      },
+    ],
+  ],
+  plugins: [
+    '@babel/proposal-object-rest-spread',
+    '@babel/proposal-class-properties',
+    'istanbul',
+  ],
+};
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,7 @@
 import Enzyme from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-15.4';
 
+const context = global;
+
 window.scrollTo = () => {};
 Enzyme.configure({ adapter: new EnzymeAdapter() });
