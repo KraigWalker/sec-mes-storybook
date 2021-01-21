b12a62b47dad98488780151397573d36218b1fad
Kraig Walker <kraig.walker@cybg.com>
2021-01-21 11:24:00 +0000

    Release: 3.3.0 (#79)
    * chore: Add pull-request-builder jenkinsfile for PR gates
    
    * chore: bump 3.0.30 to test ci pipeline (#66)
    
    * chore: bump 3.0.30 to test ci pipeline
    
    * fix: Jenkinsfile expecting cypress coverage
    
    * Feat/category feature flag (#69)
    
    * feat: Add enableCategoryAttachmentParam connfig flag
    
    * fix: Retrieve `enableCategoryAttachmentParam` from `secure_messages_webapp_metadata.flags`
    
    * chore: Bump 3.0.31 (#70)
    
    * feat: Add enableCategoryAttachmentParam connfig flag
    
    * fix: Retrieve `enableCategoryAttachmentParam` from `secure_messages_webapp_metadata.flags`
    
    * chore: bump 3.0.31
    
    * Fix: Only Add Category URL Param When Attachment === "Statements" (#72)
    
    * feat: Add enableCategoryAttachmentParam connfig flag
    
    * fix: Retrieve `enableCategoryAttachmentParam` from `secure_messages_webapp_metadata.flags`
    
    * chore: bump 3.0.31
    
    * fix: only include category param for Statements. Fixes broken "Documents" category.
    
    * Draft 3.1.0 (#73)
    
    * feat: Add enableCategoryAttachmentParam connfig flag
    
    * fix: Retrieve `enableCategoryAttachmentParam` from `secure_messages_webapp_metadata.flags`
    
    * chore: bump 3.0.31
    
    * fix: only include category param for Statements. Fixes broken "Documents" category.
    
    * chore: bump 3.1.0 – upgrade npm deps
    
    * Fix: null being appended to document url when feature flag disabled (#75)
    
    * feat: Add enableCategoryAttachmentParam connfig flag
    
    * fix: Retrieve `enableCategoryAttachmentParam` from `secure_messages_webapp_metadata.flags`
    
    * chore: bump 3.0.31
    
    * fix: only include category param for Statements. Fixes broken "Documents" category.
    
    * chore: bump 3.1.0 – upgrade npm deps
    
    * chore: upgrade document-management-lib to rc6
    
    * Bump: 3.2.0 (#76)
    
    * feat: Add enableCategoryAttachmentParam connfig flag
    
    * fix: Retrieve `enableCategoryAttachmentParam` from `secure_messages_webapp_metadata.flags`
    
    * chore: bump 3.0.31
    
    * fix: only include category param for Statements. Fixes broken "Documents" category.
    
    * chore: bump 3.1.0 – upgrade npm deps
    
    * chore: upgrade document-management-lib to rc6
    
    * chore: bump 3.2.0
    
    * ENT-425fix: add `?category=Statements` to documentID for mobile native call. (#78)
    
    * fix: add `?category=Statements` to documentID for mobile native call.
    
    * export Attachment as a named export
    
    * chore: bump v3.2.0

--- src/js/components/common/Attachment.js
+++ src/js/components/common/Attachment.js
@@ -1,5 +1,46 @@
 import { DocumentLink } from 'web-ui-components/lib/documents/navigation';
 
+function handleAttachmentClick({
+  basePath,
+  isWebView,
+  session,
+  message,
+  client,
+  getDocumentByIdNative,
+  enableCategoryAttachmentParam,
+}) {
+  const { bank_id, access_token, brand, state } = session;
+  const {
+    client: { app_title, user_tracking_id },
+  } = client;
+  const {
+    category,
+    document: { id, fileSize },
+  } = message;
+
+  if (isWebView) {
+    /**
+     * If downloading a Statement, we add a query string to the end of the documentId
+     * so that, when the native mobile builds the URL on it's end, it ends up adding
+     * the `?category=Statements` query that routes the user to the correct document
+     * store. It's a bit unorthodox, but avoids shipping a non-backawards compatible
+     * change in the native mobile apps.
+     */
+    getDocumentByIdNative(
+      category && category === 'Statements' ? `${id}?category=Statements` : id,
+      fileSize
+    );
+  } else {
+    window.open(
+      `${basePath}/my-documents/${bank_id}/${id}?bank_id=${bank_id}&client_context=${app_title}&user_tracking_id=${user_tracking_id}&brandId=${brand}&state=${state}${
+        category && category === 'Statements' && enableCategoryAttachmentParam
+          ? '&category=Statements' // Because adding the category param can throw off other types of attachments, we hardcode Statements
+          : ''
+      }#access_token=${access_token}`
+    );
+  }
+}
+
 function Attachment({
   document,
   basePath,
@@ -8,8 +49,6 @@ function Attachment({
   message,
   client,
   getDocumentByIdNative,
-  history,
-  readOnly,
 }) {
   return (
     <div className="c-message--attachments">
@@ -17,25 +56,16 @@ function Attachment({
       <DocumentLink
         linkText={document.label}
         onClick={() => {
-          if (!isWebView) {
-            handleAttachmentClick({
-              basePath,
-              isWebView,
-              session,
-              message,
-              client,
-              getDocumentByIdNative,
-              history,
-              readOnly,
-              enableCategoryAttachmentParam:
-                window.flagContext.enableCategoryAttachmentParam || false,
-            });
-          } else {
-            getDocumentByIdNative(
-              message.document.id,
-              message.document.fileSize
-            );
-          }
+          handleAttachmentClick({
+            basePath,
+            isWebView,
+            session,
+            message,
+            client,
+            getDocumentByIdNative,
+            enableCategoryAttachmentParam:
+              window.flagContext.enableCategoryAttachmentParam || false,
+          });
         }}
         hasIcon
       />
@@ -43,40 +73,4 @@ function Attachment({
   );
 }
 
-const handleAttachmentClick = ({
-  basePath,
-  session,
-  message,
-  client,
-  enableCategoryAttachmentParam,
-  // history,
-  // readOnly,
-}) => {
-  const { bank_id, access_token, brand, state } = session;
-  const {
-    client: { app_title, user_tracking_id },
-  } = client;
-  const {
-    category,
-    document: { id },
-  } = message;
-
-  // FIXME: Removed history.push in MEO as it wasn't working, need to revisit
-  // if (readOnly) { //In MEO we need to open the document on the same page due to in memory routing
-  //   history.push({
-  //       pathname: url,
-  //       backPath: '/securemessages/'
-  //     });
-  // }
-  // else {
-  window.open(
-    `${basePath}/my-documents/${bank_id}/${id}?bank_id=${bank_id}&client_context=${app_title}&user_tracking_id=${user_tracking_id}&brandId=${brand}&state=${state}${
-      category && category === 'Statements' && enableCategoryAttachmentParam
-        ? '&category=Statements' // Because adding the category param can throw off other types of attachments, we hardcode Statements
-        : ''
-    }#access_token=${access_token}`
-  );
-  // }
-};
-
-export default Attachment;
+export { Attachment };
