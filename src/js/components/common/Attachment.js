d1fac28e24143074ae5b2ebd63e3b138761180e5
Kraig Walker <kraig.walker@cybg.com>
2021-01-21 12:34:15 +0000

    Release: 3.4.0 (#81)
    * chore: Add pull-request-builder jenkinsfile for PR gates
    
    * chore: bump 3.0.30 to test ci pipeline (#66)
    
    * chore: bump 3.0.30 to test ci pipeline
    
    * fix: Jenkinsfile expecting cypress coverage
    
    * Feat/category feature flag (#69)
    
    * feat: Add enableCategoryAttachmentParam connfig flag
    
    * fix: Retrieve `enableCategoryAttachmentParam` from `secure_messages_webapp_metadata.flags`
    
    * chore: Bump 3.0.31 (#70)
    
    * feat: Add enableCategoryAttachmentParam connfig flag
    
    * fix: Retrieve `enableCategoryAttachmentParam` from `secure_messages_webapp_metadata.flags`
    
    * chore: bump 3.0.31
    
    * Fix: Only Add Category URL Param When Attachment === "Statements" (#72)
    
    * feat: Add enableCategoryAttachmentParam connfig flag
    
    * fix: Retrieve `enableCategoryAttachmentParam` from `secure_messages_webapp_metadata.flags`
    
    * chore: bump 3.0.31
    
    * fix: only include category param for Statements. Fixes broken "Documents" category.
    
    * Draft 3.1.0 (#73)
    
    * feat: Add enableCategoryAttachmentParam connfig flag
    
    * fix: Retrieve `enableCategoryAttachmentParam` from `secure_messages_webapp_metadata.flags`
    
    * chore: bump 3.0.31
    
    * fix: only include category param for Statements. Fixes broken "Documents" category.
    
    * chore: bump 3.1.0 – upgrade npm deps
    
    * Fix: null being appended to document url when feature flag disabled (#75)
    
    * feat: Add enableCategoryAttachmentParam connfig flag
    
    * fix: Retrieve `enableCategoryAttachmentParam` from `secure_messages_webapp_metadata.flags`
    
    * chore: bump 3.0.31
    
    * fix: only include category param for Statements. Fixes broken "Documents" category.
    
    * chore: bump 3.1.0 – upgrade npm deps
    
    * chore: upgrade document-management-lib to rc6
    
    * Bump: 3.2.0 (#76)
    
    * feat: Add enableCategoryAttachmentParam connfig flag
    
    * fix: Retrieve `enableCategoryAttachmentParam` from `secure_messages_webapp_metadata.flags`
    
    * chore: bump 3.0.31
    
    * fix: only include category param for Statements. Fixes broken "Documents" category.
    
    * chore: bump 3.1.0 – upgrade npm deps
    
    * chore: upgrade document-management-lib to rc6
    
    * chore: bump 3.2.0
    
    * ENT-425fix: add `?category=Statements` to documentID for mobile native call. (#78)
    
    * fix: add `?category=Statements` to documentID for mobile native call.
    
    * export Attachment as a named export
    
    * chore: bump v3.2.0
    
    * fix: display statements in webview on mobile. Bump 3.4.0 (#80)

--- src/js/components/common/Attachment.js
+++ src/js/components/common/Attachment.js
@@ -18,22 +18,19 @@ function handleAttachmentClick({
     document: { id, fileSize },
   } = message;
 
-  if (isWebView) {
-    /**
-     * If downloading a Statement, we add a query string to the end of the documentId
-     * so that, when the native mobile builds the URL on it's end, it ends up adding
-     * the `?category=Statements` query that routes the user to the correct document
-     * store. It's a bit unorthodox, but avoids shipping a non-backawards compatible
-     * change in the native mobile apps.
-     */
-    getDocumentByIdNative(
-      category && category === 'Statements' ? `${id}?category=Statements` : id,
-      fileSize
-    );
+  const isStatementDownload =
+    enableCategoryAttachmentParam && category && category === 'Statements';
+
+  /**
+   * Only trigger the native document download view if not a statement and in
+   * a native mobile app WebView
+   */
+  if (isWebView && !isStatementDownload) {
+    getDocumentByIdNative(id, fileSize);
   } else {
     window.open(
       `${basePath}/my-documents/${bank_id}/${id}?bank_id=${bank_id}&client_context=${app_title}&user_tracking_id=${user_tracking_id}&brandId=${brand}&state=${state}${
-        category && category === 'Statements' && enableCategoryAttachmentParam
+        isStatementDownload
           ? '&category=Statements' // Because adding the category param can throw off other types of attachments, we hardcode Statements
           : ''
       }#access_token=${access_token}`
