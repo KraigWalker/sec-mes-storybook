6cac842b179ffee1e7fb95204874fd725163b52e
Kraig Walker <kraig.walker@cybg.com>
2021-01-22 17:37:00 +0000

    Release: 3.5.0 (#83)
    * chore: Add pull-request-builder jenkinsfile for PR gates
    
    * chore: bump 3.0.30 to test ci pipeline (#66)
    
    * chore: bump 3.0.30 to test ci pipeline
    
    * fix: Jenkinsfile expecting cypress coverage
    
    * Feat/category feature flag (#69)
    
    * feat: Add enableCategoryAttachmentParam connfig flag
    
    * fix: Retrieve `enableCategoryAttachmentParam` from `secure_messages_webapp_metadata.flags`
    
    * chore: Bump 3.0.31 (#70)
    
    * feat: Add enableCategoryAttachmentParam connfig flag
    
    * fix: Retrieve `enableCategoryAttachmentParam` from `secure_messages_webapp_metadata.flags`
    
    * chore: bump 3.0.31
    
    * Fix: Only Add Category URL Param When Attachment === "Statements" (#72)
    
    * feat: Add enableCategoryAttachmentParam connfig flag
    
    * fix: Retrieve `enableCategoryAttachmentParam` from `secure_messages_webapp_metadata.flags`
    
    * chore: bump 3.0.31
    
    * fix: only include category param for Statements. Fixes broken "Documents" category.
    
    * Draft 3.1.0 (#73)
    
    * feat: Add enableCategoryAttachmentParam connfig flag
    
    * fix: Retrieve `enableCategoryAttachmentParam` from `secure_messages_webapp_metadata.flags`
    
    * chore: bump 3.0.31
    
    * fix: only include category param for Statements. Fixes broken "Documents" category.
    
    * chore: bump 3.1.0 – upgrade npm deps
    
    * Fix: null being appended to document url when feature flag disabled (#75)
    
    * feat: Add enableCategoryAttachmentParam connfig flag
    
    * fix: Retrieve `enableCategoryAttachmentParam` from `secure_messages_webapp_metadata.flags`
    
    * chore: bump 3.0.31
    
    * fix: only include category param for Statements. Fixes broken "Documents" category.
    
    * chore: bump 3.1.0 – upgrade npm deps
    
    * chore: upgrade document-management-lib to rc6
    
    * Bump: 3.2.0 (#76)
    
    * feat: Add enableCategoryAttachmentParam connfig flag
    
    * fix: Retrieve `enableCategoryAttachmentParam` from `secure_messages_webapp_metadata.flags`
    
    * chore: bump 3.0.31
    
    * fix: only include category param for Statements. Fixes broken "Documents" category.
    
    * chore: bump 3.1.0 – upgrade npm deps
    
    * chore: upgrade document-management-lib to rc6
    
    * chore: bump 3.2.0
    
    * ENT-425fix: add `?category=Statements` to documentID for mobile native call. (#78)
    
    * fix: add `?category=Statements` to documentID for mobile native call.
    
    * export Attachment as a named export
    
    * chore: bump v3.2.0
    
    * fix: display statements in webview on mobile. Bump 3.4.0 (#80)
    
    * Fix/optimise message summary path (#82)
    
    * fix: optimise message summary path
    
    * fix: navigate user on mobile app instead of opening a new window for Statement downloads
    
    * chore: bump 3.5.0

--- src/js/components/common/SuccessModal.js
+++ src/js/components/common/SuccessModal.js
@@ -4,40 +4,41 @@ import { ButtonGroup } from 'web-ui-components/lib/molecules/buttons';
 import { CentredModal } from 'web-ui-components/lib/organisms/modals';
 import { StandardBody } from 'web-ui-components/lib/typography/body';
 
-const SuccessModal = (props) => {
+function SuccessModal(props) {
   window.top.postMessage('clearNewMessagePage', '*');
-
-  const bodyContent = (
-    <div>
-      <div>
-        <GetIcon id="icon-success" width="68px" height="68px" fill="#9CDD00" />
-      </div>
-      {props.bodyText}
-    </div>
-  );
-
-  const footerButtons = (
-    <StandardBody>
-      <ButtonGroup alignment="center">
-        <Button display="primary" onClick={props.onClick}>
-          {props.okText}
-        </Button>
-      </ButtonGroup>
-    </StandardBody>
-  );
 
   return (
     <CentredModal
       isOpen={true}
       title=""
-      buttonNode={footerButtons}
+      buttonNode={
+        <StandardBody>
+          <ButtonGroup alignment="center">
+            <Button display="primary" onClick={props.onClick}>
+              {props.okText}
+            </Button>
+          </ButtonGroup>
+        </StandardBody>
+      }
       onClose={props.onClick}
     >
       <StandardBody>
-        <p>{bodyContent}</p>
+        <p>
+          <div>
+            <div>
+              <GetIcon
+                id="icon-success"
+                width="68px"
+                height="68px"
+                fill="#9CDD00"
+              />
+            </div>
+            {props.bodyText}
+          </div>
+        </p>
       </StandardBody>
     </CentredModal>
   );
-};
+}
 
-export default SuccessModal;
+export { SuccessModal };
